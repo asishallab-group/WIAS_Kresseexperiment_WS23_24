a8e2b790434d7c119418ca0df1d887028947e1ad
Asis Hallab <asis.hallab@gmail.com>
2023-11-09 16:01:51 +0100

    Started writing python code to simulate plant heights according to different growth media.

---
+++ Methods/simulate_data.py
@@ -0,0 +1,51 @@
+# Simulate data for the plotting scripts, so we can plot before any meaurements
+# (data) are actually available.
+
+# Wir brauchen einige Werkzeuge, die uns helfen Zufallszahlen zu generieren.
+import numpy
+# Wir brauchen eine Bibliothek, um die simulierten Daten in einer Tabelle
+# (comma seperated values, CSV) abzuspeichern:
+import pandas
+
+# Wir generieren uns Zufallszahlen, die Gauss (Normal) verteilt sind.
+
+def generate_plant_heights(n_per_day, mean, standard_deviation, add_to_mean_per_day, medium, student):
+    """
+    Generate (simulate) plant height measurements using normally distributed
+    random values. Generate argument 'n_per_day' random numbers for each day of
+    1 to 14 and add to the mean of the normal distribution each day the
+    argument 'add_to_mean_per_day'.
+
+    :param n_per_day: Defines how many random vavlues per day are generated
+    :param mean: The mean value of the normal distribution on day one
+    :param standard_deviation: The standard deviation used for the random number generation with numpy.random.normal
+    :param add_to_mean_per_day: Each day this value will be added to the mean of the day before.
+    :param medium: A string describing the medium the plants were grown in
+    :param student: A string, the name of the student that carried out the measurements
+    :return: Return an array of arrays, where each row (array) represents a row
+    in the plant height measurement table
+    """
+    random_plant_heights_per_day = []
+    for day in range(1,15):
+        day_mean = mean + (day - 1) * add_to_mean_per_day
+        day_random_plant_heights = numpy.random.normal(day_mean, standard_deviation, n_per_day)
+        for random_plant_height in day_random_plant_heights:
+            # This is a single row in our simulated plant height measurements
+            # output table:
+            random_plant_heights_per_day.append(
+                    {'Medium': medium, 'Height': random_plant_height, 'Day': day,
+                     'Student': student}
+                )
+    return random_plant_heights_per_day
+
+
+# Generate plant heights for normal medium
+normal_medium_plant_height_sim = generate_plant_heights(20, 3, 2, 2)
+# Generate plant heights for salty medium
+salty_medium_plant_height_sim = generate_plant_heights(20, 2, 1.5, 1)
+# Generate plant heights for acidic medium
+acidic_medium_plant_height_sim = generate_plant_heights(20, 1, 1, 0.5)
+
+# Write the simulated plant height measurements into a table of the same format
+# that will be used to deliver the actual measurements:
+# To Do write the code to save the table.
--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 In diesem Git Repository finden wir alle Materialien, Methoden und Ergebnisse
 zu dem Forschungsprojekt.
+
+## Simulate data
+
+Wir möchten unsere Plot erzeugenden Scripte vorbereiten, bevor schon
+Pflanzenwuchshöhen gemessen worden sind. Um dies zu ermöglichen, simulieren wir
+uns Wuchshöhendaten. Dies machen wir mit dem Script:
+`./Methods/simulate_data.py`
+
+_Work in progress. See file simulate_data.py_
